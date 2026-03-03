157bb83b9c8e1d22debd489d39320e6d0f28f274
rotmarer23 <167620937+Timyr2012@users.noreply.github.com>
2026-03-03 11:30:42 +0200

    Add files via upload

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Вітаємо на регестрації, впишіть своє ім'я");
+            string name = Console.ReadLine();
+            Console.WriteLine("впишіть свій вік");
+            string age = Console.ReadLine();
+            Console.WriteLine("Придумайте пароль");
+            string password = Console.ReadLine();
+                      
+            Console.WriteLine($"Вітаємо {name} в нашому клубі, ми раді що ви в свої {age} приєдналися до нас. та нікоми не кажіть що пароль {password}");
+
+
+        }
+    }
+}
